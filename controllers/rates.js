6e361ec56497aa227a000beb6c14d3ef95c67837
StefanoSilv <silver.stefano@gmail.com>
2020-04-17 16:46:15 +0200

    getting rates from the api

--- controllers/rates.js
+++ controllers/rates.js
@@ -1,8 +1,26 @@
 const Rates = require('../models/rates');
 const router = require('express').Router();
+const axios = require('axios');
+
+const getPastDays = number => {
+	let date = new Date();
+	let pastDate = new Date(date.setDate(date.getDate() - number)).toISOString();
+	return pastDate;
+};
 
 router.post('/', (req, res) => {
-	console.log('hello');
+	for (let i = 0; i < 7; i++) {
+		axios
+			.get(
+				`http://api.coinlayer.com/${getPastDays(i)
+					.toString()
+					.substr(0, 10)}?access_key=${
+					process.env.REACT_APP_COINLAYER_KEY
+				}&target=EUR&symbols=BTC,ETH,XRP,BCH,EOS,LTC`
+			)
+			.then(res => {
+				console.log('res.data', res.data);
+			});
+	}
 });
-
 module.exports = router;
