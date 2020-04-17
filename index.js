e7461f5273c9bd4faff8895b3501305dcaa7f3d3
StefanoSilv <silver.stefano@gmail.com>
2020-04-17 16:23:40 +0200

    server running

---
+++ index.js
@@ -0,0 +1,19 @@
+// Packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+
+app.use(cors({ credentials: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+	console.log(`Ready on port ${process.env.PORT}`);
+});
+
+module.exports = app;
