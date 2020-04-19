9dbb6ad11b534b57dc54ac13c150343a5d1f6600
StefanoSilv <silver.stefano@gmail.com>
2020-04-19 17:14:48 +0200

    functions to retrieve datas added

--- functions.js
+++ functions.js
@@ -5,11 +5,63 @@ module.exports = {
 		axios
 			.get(`http://localhost:4020/rates?date=${date}`)
 			.then(res => {
-				console.log(res.data);
-				res.send({ rate: res.data });
+				return res.data;
 			})
 			.catch(err => {
-				res.send({ err });
+				return { err };
+			});
+	},
+	getByPastDays: number => {
+		let date = new Date();
+		let pastDate = new Date(
+			date.setDate(date.getDate() - number)
+		).toISOString();
+		axios
+			.get(
+				`http://localhost:4020/rates?date=${pastDate.toString().substr(0, 10)}`
+			)
+			.then(res => {
+				console.log(res.data);
+				return res.data;
+			})
+			.catch(err => {
+				return { err };
+			});
+	},
+	getPastMonth: () => {
+		let date = new Date();
+		let pastDate = new Date(date.setDate(date.getDate())).toISOString();
+		let year = pastDate.toString().substr(0, 5);
+		let month = (Number(pastDate.toString().substr(5, 2)) - 1)
+			.toString()
+			.padStart(2, '0');
+		let day = pastDate.toString().substr(7, 3);
+		let dateQuery = year + month + day;
+		axios
+			.get(`http://localhost:4020/rates?date=${dateQuery}`)
+			.then(res => {
+				console.log(res.data);
+				return res.data;
+			})
+			.catch(err => {
+				return { err };
+			});
+	},
+	getPastYear: () => {
+		let date = new Date();
+		let pastDate = new Date(date.setDate(date.getDate())).toISOString();
+		let year = (Number(pastDate.toString().substr(0, 4)) - 1).toString();
+		let month = pastDate.toString().substr(4, 3);
+		let day = pastDate.toString().substr(7, 3);
+		let dateQuery = year + month + day;
+		axios
+			.get(`http://localhost:4020/rates?date=${dateQuery}`)
+			.then(res => {
+				console.log(res.data);
+				return res.data;
+			})
+			.catch(err => {
+				return { err };
 			});
 	}
 };
