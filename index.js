0f91f50e77316ddc1197cb0a0b57431b2d39652b
StefanoSilv <silver.stefano@gmail.com>
2020-04-23 10:57:05 +0200

    cors moved in index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const cors = require('cors');
 require('dotenv').config();
 const exchange = require('./functions');
 
-const app = express();
-const database = require('./db');
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const app = express();
+const database = require('./db');
+
 app.use('/rates', require('./controllers/exchange_rates'));
 
 // Comment out the below routes if the code was pulled from git
-// app.use('/simpleRates', require('./controllers/rates'));
-app.use('/live', require('./controllers/live'));
+app.use('/simpleRates', require('./controllers/rates'));
+// app.use('/live', require('./controllers/live'));
 setInterval(() => require('./controllers/live')(), 2 * 1000 * 60 * 60);
 
 // Middleware
